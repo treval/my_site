001ac30c46d7853c6fddff73a9ebeecc13d8e40f
Trevor Levey <trvlev@gmail.com>
2019-08-20 19:01:23 -0400

    Updates

---
+++ precache-manifest.2506f8ee264f97110b1fd418b01cc956.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4728e60b87f9f28ebc20e4aa905b236",
+    "url": "/my_site/index.html"
+  },
+  {
+    "revision": "fd71b7381ff49b45dc1d",
+    "url": "/my_site/static/css/2.e8d046a2.chunk.css"
+  },
+  {
+    "revision": "83706ae003021d8778f4",
+    "url": "/my_site/static/css/main.9ccad731.chunk.css"
+  },
+  {
+    "revision": "fd71b7381ff49b45dc1d",
+    "url": "/my_site/static/js/2.0c1c641c.chunk.js"
+  },
+  {
+    "revision": "83706ae003021d8778f4",
+    "url": "/my_site/static/js/main.b1ebb440.chunk.js"
+  },
+  {
+    "revision": "36b4bd527530cc9c02e5",
+    "url": "/my_site/static/js/runtime~main.2c65e842.js"
+  }
+]);
