f0c8197b5a95b19baabfe2dc795a7d42aa01bcc0
Trevor Levey <trvlev@gmail.com>
2019-08-20 18:28:47 -0400

    initial commit

---
+++ precache-manifest.69506a3c110e727e051f3c84ba3ee0ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09ca5d3bfa82473353a2ed3b8693b8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd71b7381ff49b45dc1d",
+    "url": "/static/css/2.e8d046a2.chunk.css"
+  },
+  {
+    "revision": "b1cb2d3e95e59ef7d1e1",
+    "url": "/static/css/main.9ccad731.chunk.css"
+  },
+  {
+    "revision": "fd71b7381ff49b45dc1d",
+    "url": "/static/js/2.0c1c641c.chunk.js"
+  },
+  {
+    "revision": "b1cb2d3e95e59ef7d1e1",
+    "url": "/static/js/main.83f95e5f.chunk.js"
+  },
+  {
+    "revision": "06f33b6cd92d7b2e90d5",
+    "url": "/static/js/runtime~main.597bbc11.js"
+  }
+]);
